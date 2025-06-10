5e47006d38b09c43dc1a462c1d2af797ec4ac2b1
Hung127 <hunghungtt05@gmail.com>
2025-06-10 18:31:52 +0700

    Added resize, fixed some bugs about sizeLog

--- FibHash.h
+++ FibHash.h
@@ -1,5 +1,5 @@
-#ifndef FIB_HASH
-#define FIB_HASH
+#ifndef FIB_HASH_H
+#define FIB_HASH_H
 #include <iostream>
 #include <vector>
 #define uint unsigned int
@@ -9,7 +9,7 @@
 uint getLog2(uint num) {
     // every number needs at least 1 bit to represent
     uint count = 1;
-    while ((num >> count) > 0) {
+    while ((num >> count) > 1) {
         count++;
     }
     return count;
@@ -35,18 +35,40 @@ private:
     std::vector<Entry> table;
 
     uint hashFunction(uint key) {
+        // use ((unsigned long long) key * FIB_CONST) & ((1ull << WORD_SIZE) - 1) to make sure 
+        // that it will always fits int 32 unsigned bits (number <= 2^32 - 1)
         return (((unsigned long long) key * FIB_CONST) & ((1ull << WORD_SIZE) - 1)) >> (WORD_SIZE - sizeLog);
+    }
+
+    void resize(uint newSize, int newSizeLog) {
+        // create a new vector to hold old keys
+        std::vector<Entry> oldTable(table);
+        size = newSize;
+        sizeLog = newSizeLog;
+        // create a new table
+        table = std::vector<Entry>(newSize);
+        count = 0;
+        for (const Entry &e : oldTable) {
+            // empty slot or deleted slot
+            if (e.isDeleted || !e.isOccupied) {
+                continue;
+            }
+            if (!insert(e.key)) {
+                std::cout << "FAULT with key " << e.key << "\n";
+                return;
+            }
+        }
     }
 
 public:
     bool insert(uint key) {
-        if (count >= size) {
-            return false;
+        if (2 * count >= size) {
+            resize(size * 2, sizeLog + 1);
         }
 
         uint hash = hashFunction(key);
         bool firstDeleted = false;
-        uint firstDeletedIdx;
+        uint firstDeletedIdx = 0;
 
         if (!table[hash].isOccupied) {
             table[hash].key = key;
@@ -77,7 +99,9 @@ public:
         }
 
         // make sure that always add key to deleted place first (if can)
-        if (firstDeleted) i = firstDeletedIdx;
+        if (firstDeleted) {
+            i = firstDeletedIdx;
+        }
 
         // add to empty slot or deleted slot
         table[i].key = key;
@@ -142,8 +166,15 @@ public:
         return false;
     }
 
-    // assume that tableSize always be a power of 2
-    FibonacciHashTable(uint tableSize) : size(tableSize), sizeLog(getLog2(tableSize)), table(1u << sizeLog), count(0) {}
+    // assume that tableSize is always be a power of 2
+    FibonacciHashTable(uint tableSize) : size(std::max(1u, tableSize)), sizeLog(getLog2(tableSize)), table(std::max(1u, tableSize)), count(0) {
+        // int temp = sizeLog;
+        // sizeLog = 3;
+        // for (uint i = 0; i < 10; i++) {
+        //     std::cout << hashFunction(i) << " " << i << "\n";
+        // }
+        // sizeLog = temp;
+    }
 };
 
 #endif
